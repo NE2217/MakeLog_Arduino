3735f6f832d48e0b8b1a652b964ecda879bbaded
Nikita <niker217@mail.ru>
2024-01-11 16:04:34 +0700

    -добавлен второй выход, исправлен контроль цикла и форма записи в UART

--- ArduinoProject/AnalogAutput/AnalogAutput.ino
+++ ArduinoProject/AnalogAutput/AnalogAutput.ino
@@ -1,44 +1,51 @@
-#include <MsTimer2.h>
-
 #define MEASURE_PERIOD 500  // время периода измерения
-#define BITNESS_ANALOG 1024 // разрядность сигнала
+#define ADC_RAW_MAX 1024    // разрядность сигнала
 #define VOLTAGE_MAX 5.0     // максимальное напряжение на входе
 
-int TimeCount;      // счетчик времени
-int InputCod;       // код входного напряжения
-float InputVoltage; // входное напряжение в В
-float GetMilS;       // Счетчик миллисекунд
+#define VOLTAGE_CALC(raw) raw * VOLTAGE_MAX / ADC_RAW_MAX //получение напряжения на АЦП
 
-void  timerInterupt() {
-  TimeCount++;
+uint16_t AdcRaw;                // код входного напряжения
+float InputVoltage;             // входное напряжение в В
+
+uint32_t LocalTime = millis();  // Счетчик миллисекунд
+
+void f_TimeReset()
+{
+  LocalTime=millis();
+}
+
+bool f_IsTimeOut (){
+  if(millis() - LocalTime == MEASURE_PERIOD)
+  {
+    f_TimeReset();
+    return true;
+  }
+  return false;
 }
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(A0, INPUT);                 // установка режима аналогового входа для A0
+  pinMode(A0, INPUT);                   // установка режима аналогового входа для A0
+  pinMode(A1, INPUT);                   // установка режима аналогового входа для A1
 
-  Serial.begin(9600);                 // инициализируем порт, скорость 9600
-  MsTimer2::set(1, timerInterupt);    // прерывания по таймеру, период 1 мс
-  MsTimer2::start();                  // разрешение прерывания
+  Serial.begin(9600);                   // инициализируем порт, скорость 9600
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if ( TimeCount >= MEASURE_PERIOD ) {
-    TimeCount = 0;
+  uint32_t TimeStamp = millis();
+
+  if ( f_IsTimeOut() ) 
+  {
     
-    InputCod = analogRead(A0);                                         // чтение напряжения на входе A3
-    InputVoltage = ( (float)InputCod * VOLTAGE_MAX / BITNESS_ANALOG ); // пересчет кода в напряжение (В)
+    AdcRaw = analogRead(A0);                                         // чтение напряжения на входе A3
+    InputVoltage = VOLTAGE_CALC( (float)AdcRaw );                    // пересчет кода в напряжение (В)
 
      // передача данных через последовательный порт
-    //Serial.print("/"U":/"); Serial.print("/"%d"/", inputVoltage, 2);  Serial.print("\r\n");   Serial.print(" {\"U=\": \"%d\",\" I\": \"%d\"} ", inputVoltage, (inputVoltage/2), 2, 2 );
-     GetMilS = millis();
-     Serial.write("("); Serial.print(GetMilS, 0); Serial.write(") ");
-     Serial.write("{\"U\": \""); Serial.print(InputVoltage*100, 2); Serial.write("\"}"); Serial.print("\r\n");
-     
-     GetMilS = millis();
-     Serial.write("("); Serial.print(GetMilS, 0); Serial.write(") ");
+     Serial.write("{\"TimeStamp\": \""); Serial.print(TimeStamp, DEC); Serial.write("\"} ");
+     Serial.write("{\"U\": \""); Serial.print(InputVoltage*100, 2); Serial.write("\"} ");
      Serial.write("{\"I\": \""); Serial.print(InputVoltage, 2); Serial.write("\"}"); Serial.print("\r\n");
+
   }  
 }
