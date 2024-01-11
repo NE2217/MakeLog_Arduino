a5e9399459d9de493ed93b615c1069fd30abdfee
Nikita <niker217@mail.ru>
2024-01-11 16:53:53 +0700

    -Сигнал по 2 каналам, функции пересчета

--- ArduinoProject/AnalogAutput/AnalogAutput.ino
+++ ArduinoProject/AnalogAutput/AnalogAutput.ino
@@ -1,11 +1,15 @@
+//ОНО
 #define MEASURE_PERIOD 500  // время периода измерения
 #define ADC_RAW_MAX 1024    // разрядность сигнала
-#define VOLTAGE_MAX 5.0     // максимальное напряжение на входе
+#define VOLTAGE_MAX 5.0     // максимальное напряжение на входе, В
+#define CURRENT_MAX 100     // максимальное значение тока на входе, мА
 
 #define VOLTAGE_CALC(raw) raw * VOLTAGE_MAX / ADC_RAW_MAX //получение напряжения на АЦП
+#define PARAM_CALC(raw, max) raw * max / ADC_RAW_MAX      //получение pyfxtybz d cjjndtncndbb 
 
 uint16_t AdcRaw;                // код входного напряжения
 float InputVoltage;             // входное напряжение в В
+float InputCurrent;              // входной ток в мА
 
 uint32_t LocalTime = millis();  // Счетчик миллисекунд
 
@@ -39,13 +43,14 @@ void loop() {
   if ( f_IsTimeOut() ) 
   {
     
-    AdcRaw = analogRead(A0);                                         // чтение напряжения на входе A3
-    InputVoltage = VOLTAGE_CALC( (float)AdcRaw );                    // пересчет кода в напряжение (В)
-
+    AdcRaw = analogRead(A0);                                 // чтение напряжения на входе A0
+    InputVoltage = VOLTAGE_CALC( (float)AdcRaw );            // пересчет кода в напряжение (В)
+    AdcRaw = analogRead(A1);                                 // чтение напряжения на входе A0
+    InputCurrent = PARAM_CALC( (float)AdcRaw, CURRENT_MAX);  // пересчет кода в ток (мА)
      // передача данных через последовательный порт
      Serial.write("{\"TimeStamp\": \""); Serial.print(TimeStamp, DEC); Serial.write("\"} ");
-     Serial.write("{\"U\": \""); Serial.print(InputVoltage*100, 2); Serial.write("\"} ");
-     Serial.write("{\"I\": \""); Serial.print(InputVoltage, 2); Serial.write("\"}"); Serial.print("\r\n");
+     Serial.write("{\"U\": \""); Serial.print(InputVoltage, 2); Serial.write("\"} ");
+     Serial.write("{\"I\": \""); Serial.print(InputCurrent/InputVoltage, 2); Serial.write("\"}"); Serial.print("\r\n");
 
   }  
 }
